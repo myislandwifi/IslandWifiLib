fb31b43d57cddd92d54bb19d4ff18801ce17b3e4
myislandwifi <34301761+myislandwifi@users.noreply.github.com>
2018-03-28 23:02:56 -0500

    asf

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,21 @@
 
-export * from './models/transaction'
+export * from './models/agentActivity';
+export * from './models/assetActivity';
+export * from './models/assetState';
+export * from './models/bill';
+export * from './models/cardinfo';
+export * from './models/contract';
+export * from './models/customer';
+export * from './models/islandWifiAsset';
+export * from './models/queryConfig';
+export * from './models/refund';
+export * from './models/settings';
+export * from './models/StoreLocation';
+export * from './models/stripeCharge';
+export * from './models/stripePaymentSource';
+export * from './models/stripeSubscription';
+export * from './models/transaction';
+export * from './models/user';
 
 /*class Contract {
     constructor(id,createdDate,endsAt,agentId) {
